86c890f726d0bca7c3804a5a4d39427394cc983c
Nicolas Isnardi <nicolasisnardi@gmail.com>
2017-09-02 15:51:49 -0300

    Add publics

--- index.js
+++ index.js
@@ -1,25 +1,32 @@
 const express = require('express')
 const app = express()
-var api = require('./api')
+const necesidades = require('./necesidades')
+const path = require('path')
+const api = require('./api')
+
+function getFilePath(file) {
+  return path.join(__dirname + '/files/' + file)
+}
 
 app.set('port', (process.env.PORT || 5000));
-
 app.use(express.static(__dirname + '/public'));
 
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+app.get('/', function(req, res) {
+  res.sendFile(getFilePath('index.html'));
+});
 
-app.use('/api', api)
-
-// app.get('/', function(request, response) {
-//   response.render('pages/index');
-// });
-
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+app.post('/', function (req, res) {
+  res.send('Got a POST request')
 })
 
+app.get('/api/necesidades', function (req, res) {
+  res.json(necesidades)
+})
+
+app.post('/api/necesidades', function (req, res) {
+  console.log(req.params);
+  res.json(necesidades)
+})
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
